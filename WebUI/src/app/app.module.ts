4319adf3f19529c1ac6f634afa6b7c5c0bf6a9ce
ab_cool88 <ab_cool88@hotmail.com>
2017-05-16 21:50:59 -0500

    worked on routing and still has to do.

--- WebUI/src/app/app.module.ts
+++ WebUI/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+
+// Routes
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    AppComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule
