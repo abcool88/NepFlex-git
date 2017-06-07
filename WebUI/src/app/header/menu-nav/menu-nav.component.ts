eed584248b4058294555019fb1dac58a8b5624dd
ab_cool88 <ab_cool88@hotmail.com>
2017-06-06 22:33:45 -0500

    card4_ router with pop menu need database on next

--- WebUI/src/app/header/menu-nav/menu-nav.component.ts
+++ WebUI/src/app/header/menu-nav/menu-nav.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuNavComponent implements OnInit {
   navList: any;
+  womenMenuPopContainer: any;
   expandNoItem: boolean;
   expandUserStatus: boolean;
+
+
   constructor() {
     this.expandNoItem = false;
     this.expandUserStatus = false;
@@ -20,6 +23,28 @@ export class MenuNavComponent implements OnInit {
       { id: '5', name: 'LATEST ARRIVAL' },
       { id: '6', name: 'ELECTRONICS' }
     ];
+
+    this.womenMenuPopContainer = [
+      { id: '1', label: 'activewear', menulink: 'women', title: 'all', url: '/shoppingbag' },
+      { id: '2', label: 'blazers', menulink: 'women', title: '', url: '/billingandshipping' },
+      { id: '3', label: 'coats', menulink: 'women', title: '', url: '/shoppingbag' },
+      { id: '4', label: 'Dresses', menulink: 'women', title: '', url: '' },
+      { id: '5', label: 'Jackets', menulink: 'women', title: '', url: '' },
+      { id: '6', label: 'Jeans', menulink: 'women', title: '', url: '' },
+      { id: '7', label: 'Jumpsuits &Rompers', menulink: 'women', title: '', url: '' },
+      { id: '8', label: 'Leggings', menulink: 'women', title: '', url: '' },
+      { id: '9', label: 'maternity', menulink: 'women', title: '', url: '' },
+      { id: '10', label: 'nighty', menulink: 'women', title: '', url: '' },
+      { id: '11', label: 'pajamas', menulink: 'women', title: '', url: '' },
+      { id: '12', label: 'pants', menulink: 'women', title: '', url: '' },
+      { id: '13', label: 'shorts', menulink: 'women', title: '', url: '' },
+      { id: '14', label: 'skirts', menulink: 'women', title: '', url: '' },
+      { id: '15', label: 'suits', menulink: 'women', title: '', url: '' },
+      { id: '16', label: 'sweaters', menulink: 'women', title: '', url: '' },
+      { id: '17', label: 'swimwear', menulink: 'women', title: '', url: '' },
+      { id: '18', label: 'socks', menulink: 'women', title: '', url: '' },
+      { id: '19', label: 'tops', menulink: 'women', title: '', url: '' }
+    ]
   }
 
   ngOnInit() {
